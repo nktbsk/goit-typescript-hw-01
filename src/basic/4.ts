3d5d2017c7f3cc25d3bcdbafde085ca5dec2f3aa
nktbsk <nikita.bashkir01@gmail.com>
2024-11-18 11:33:35 +0200

    fix basic file 3 & 4

--- src/basic/4.ts
+++ src/basic/4.ts
@@ -1,12 +1,12 @@
 //? Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
 
 
-function showMessage(message: string) {
+function showMessage(message: string): void {
   console.log(message);
 }
 showMessage("Hello World!");
 
-function calc(num1: number, num2: number) {
+function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
 console.log("calc", calc(1, 2));
